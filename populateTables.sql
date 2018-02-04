2a38023ef7bbf9b408707f4322b49ffa0ce4cfc5
Jasmine Dacones <jasminedacones@gmail.com>
2018-02-04 04:17:41 -0800

    Populated tables with six movies.

--- populateTables.sql
+++ populateTables.sql
@@ -17,3 +17,93 @@ INSERT INTO DIRECTORS VALUES (5000, 'Jordan', 'Peele');
 INSERT INTO ROLES VALUES (5000, 100, 'Daniel', 'Kaluuya', 'Chris Washington');
 INSERT INTO ROLES VALUES (5000, 101, 'Allison', 'Williams', 'Rose Armitage');
 INSERT INTO ROLES VALUES (5000, 102, 'Catherine', 'Keener', 'Missy Armitage');	
+
+--REM MOVIE 2 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5001, 'Jumanji: Welcome to the Jungle', 'Four teenagers are sucked into a magical video game, and the only way they can escape is to work together to finish the game.', 119, 'PG-13', 76, 7.2, 2017);
+
+INSERT INTO GENRES VALUES (5001, 'Action');
+INSERT INTO GENRES VALUES (5001, 'Adventure');
+INSERT INTO GENRES VALUES (5001, 'Comedy');
+
+INSERT INTO LOCATIONS VALUES (5001, 'Theaters');
+
+INSERT INTO DIRECTORS VALUES (5001, 'Jake', 'Kasdan');
+
+INSERT INTO ROLES VALUES (5001, 103, 'Dwayne', 'Johnson', 'Spencer');
+INSERT INTO ROLES VALUES (5001, 104, 'Kevin', 'Hart', 'Fridge');
+INSERT INTO ROLES VALUES (5001, 105, 'Jack', 'Black', 'Bethany');
+INSERT INTO ROLES VALUES (5001, 106, 'Karen', 'Gillan', 'Martha');
+INSERT INTO ROLES VALUES (5001, 107, 'Nick', 'Jonas', 'Alex');
+
+--REM MOVIE 3 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5002, 'Deadpool', 'A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.', 108, 'R', 83, 8.0, 2016);
+
+INSERT INTO GENRES VALUES (5002, 'Action');
+INSERT INTO GENRES VALUES (5002, 'Adventure');
+INSERT INTO GENRES VALUES (5002, 'Comedy');
+
+INSERT INTO LOCATIONS VALUES (5002, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5002, 'Google Play');
+INSERT INTO LOCATIONS VALUES (5002, 'YouTube');
+
+INSERT INTO DIRECTORS VALUES (5002, 'Tim', 'Miller');
+
+INSERT INTO ROLES VALUES (5002, 108, 'Ryan', 'Reynolds', 'Wade');
+INSERT INTO ROLES VALUES (5002, 109, 'TJ', 'Miller', 'Weasel');
+
+--REM MOVIE 4 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5003, 'Thor: Ragnarok', 'Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the destruction of his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.', 130, 'PG-13', 82, 8.0, 2017);
+
+INSERT INTO GENRES VALUES (5003, 'Action');
+INSERT INTO GENRES VALUES (5003, 'Adventure');
+INSERT INTO GENRES VALUES (5003, 'Comedy');
+
+INSERT INTO LOCATIONS VALUES (5003, 'Theaters');
+
+INSERT INTO DIRECTORS VALUES (5003, 'Taika', 'Waititi');
+
+INSERT INTO ROLES VALUES (5003, 110, 'Chris', 'Hemsworth', 'Thor');
+INSERT INTO ROLES VALUES (5003, 111, 'Tom', 'Hiddleston', 'Loki');
+INSERT INTO ROLES VALUES (5003, 112, 'Benedict', 'Cumberbatch', 'Doctor Strange');	
+
+--REM MOVIE 5 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5004, 'Blade Runner 2049', 'A young blade runner''s discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who''s been missing for thirty years.', 164, 'R', 87, 8.2, 2017);
+
+INSERT INTO GENRES VALUES (5004, 'Drama');
+INSERT INTO GENRES VALUES (5004, 'Mystery');
+INSERT INTO GENRES VALUES (5004, 'Sci-Fi');
+
+INSERT INTO LOCATIONS VALUES (5004, 'Theaters');
+
+INSERT INTO DIRECTORS VALUES (5004, 'Denis', 'Villeneuve');
+
+INSERT INTO ROLES VALUES (5004, 113, 'Ryan', 'Gosling', 'K');
+INSERT INTO ROLES VALUES (5004, 114, 'Ana de', 'Armas', 'Joi');
+INSERT INTO ROLES VALUES (5004, 115, 'Harrison', 'Ford', 'Rick Deckard');
+
+--REM MOVIE 6 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5005, 'Maze Runner: The Death Cure', 'Young hero Thomas embarks on a mission to find a cure for a deadly disease known as the "Flare".', 141, 'PG-13', 43, 7.0, 2017);
+
+INSERT INTO GENRES VALUES (5005, 'Action');
+INSERT INTO GENRES VALUES (5005, 'Sci-Fi');
+INSERT INTO GENRES VALUES (5005, 'Thriller');
+
+INSERT INTO LOCATIONS VALUES (5005, 'Theaters');
+
+INSERT INTO DIRECTORS VALUES (5005, 'Wes', 'Ball');
+
+INSERT INTO ROLES VALUES (5005, 116, 'Dylan', 'O''Brien', 'Thomas');
+INSERT INTO ROLES VALUES (5005, 117, 'Ki Hong', 'Lee', 'Minho');
+INSERT INTO ROLES VALUES (5005, 118, 'Kaya', 'Scodelario', 'Teresa');
+
+
