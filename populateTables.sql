e7dc9acfa995169833cacff07784bcbd610d6b6b
Jasmine Dacones <jasminedacones@gmail.com>
2018-02-05 09:59:23 -0800

    Populated the tables with the rest of the data.

--- populateTables.sql
+++ populateTables.sql
@@ -1,7 +1,10 @@
 ﻿--REM MOVIE 1 ========================================================
 
 INSERT INTO 	MOVIES
-VALUES			(5000, 'Get Out', 'It''s time for a young African-American to meet with his white girlfriend''s parents for a weekend in their secluded estate in the woods, but before long, the friendly and polite ambience will give way to a nightmare', 104, 'R', 99, 7.7, 2017);
+VALUES			(5000, 'Get Out', 'It''s time for a young African-American to meet with his white
+				girlfriend''s parents for a weekend in their secluded estate in the woods, but 
+				before long, the friendly and polite ambience will give way to a nightmare', 
+				104, 'R', 99, 7.7, 2017);
 
 INSERT INTO GENRES VALUES (5000, 'Horror');
 INSERT INTO GENRES VALUES (5000, 'Mystery');
@@ -21,7 +24,9 @@ INSERT INTO ROLES VALUES (5000, 102, 'Catherine', 'Keener', 'Missy Armitage');
 --REM MOVIE 2 ========================================================
 
 INSERT INTO 	MOVIES
-VALUES			(5001, 'Jumanji: Welcome to the Jungle', 'Four teenagers are sucked into a magical video game, and the only way they can escape is to work together to finish the game.', 119, 'PG-13', 76, 7.2, 2017);
+VALUES			(5001, 'Jumanji: Welcome to the Jungle', 'Four teenagers are sucked into a magical
+				video game, and the only way they can escape is to work together to finish the game.', 
+				119, 'PG-13', 76, 7.2, 2017);
 
 INSERT INTO GENRES VALUES (5001, 'Action');
 INSERT INTO GENRES VALUES (5001, 'Adventure');
@@ -40,7 +45,9 @@ INSERT INTO ROLES VALUES (5001, 107, 'Nick', 'Jonas', 'Alex');
 --REM MOVIE 3 ========================================================
 
 INSERT INTO 	MOVIES
-VALUES			(5002, 'Deadpool', 'A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.', 108, 'R', 83, 8.0, 2016);
+VALUES			(5002, 'Deadpool', 'A fast-talking mercenary with a morbid sense of humor is
+				subjected to a rogue experiment that leaves him with accelerated healing powers 
+				and a quest for revenge.', 108, 'R', 83, 8.0, 2016);
 
 INSERT INTO GENRES VALUES (5002, 'Action');
 INSERT INTO GENRES VALUES (5002, 'Adventure');
@@ -58,7 +65,10 @@ INSERT INTO ROLES VALUES (5002, 109, 'TJ', 'Miller', 'Weasel');
 --REM MOVIE 4 ========================================================
 
 INSERT INTO 	MOVIES
-VALUES			(5003, 'Thor: Ragnarok', 'Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the destruction of his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.', 130, 'PG-13', 82, 8.0, 2017);
+VALUES			(5003, 'Thor: Ragnarok', 'Thor is imprisoned on the other side of the universe
+				and finds himself in a race against time to get back to Asgard to stop Ragnarok, 
+				the destruction of his homeworld and the end of Asgardian civilization, at the 
+				hands of an all-powerful new threat, the ruthless Hela.', 130, 'PG-13', 82, 8.0, 2017);
 
 INSERT INTO GENRES VALUES (5003, 'Action');
 INSERT INTO GENRES VALUES (5003, 'Adventure');
@@ -75,7 +85,9 @@ INSERT INTO ROLES VALUES (5003, 112, 'Benedict', 'Cumberbatch', 'Doctor Strange'
 --REM MOVIE 5 ========================================================
 
 INSERT INTO 	MOVIES
-VALUES			(5004, 'Blade Runner 2049', 'A young blade runner''s discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who''s been missing for thirty years.', 164, 'R', 87, 8.2, 2017);
+VALUES			(5004, 'Blade Runner 2049', 'A young blade runner''s discovery of a long-buried 
+				secret leads him to track down former blade runner Rick Deckard, who''s been 
+				missing for thirty years.', 164, 'R', 87, 8.2, 2017);
 
 INSERT INTO GENRES VALUES (5004, 'Drama');
 INSERT INTO GENRES VALUES (5004, 'Mystery');
@@ -92,7 +104,9 @@ INSERT INTO ROLES VALUES (5004, 115, 'Harrison', 'Ford', 'Rick Deckard');
 --REM MOVIE 6 ========================================================
 
 INSERT INTO 	MOVIES
-VALUES			(5005, 'Maze Runner: The Death Cure', 'Young hero Thomas embarks on a mission to find a cure for a deadly disease known as the "Flare".', 141, 'PG-13', 43, 7.0, 2017);
+VALUES			(5005, 'Maze Runner: The Death Cure', 'Young hero Thomas embarks on a mission
+				to find a cure for a deadly disease known as the "Flare".', 141, 'PG-13', 43, 
+				7.0, 2017);
 
 INSERT INTO GENRES VALUES (5005, 'Action');
 INSERT INTO GENRES VALUES (5005, 'Sci-Fi');
@@ -106,4 +120,201 @@ INSERT INTO ROLES VALUES (5005, 116, 'Dylan', 'O''Brien', 'Thomas');
 INSERT INTO ROLES VALUES (5005, 117, 'Ki Hong', 'Lee', 'Minho');
 INSERT INTO ROLES VALUES (5005, 118, 'Kaya', 'Scodelario', 'Teresa');
 
+--REM MOVIE 7 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5006, 'It', 'In the summer of 1989, a group of bullied kids band together to 
+				destroy a shapeshifting monster, which disguises itself as a clown and preys on 
+				the children of Derry, their small Maine town.', 135, 'R', 85, 7.5, 2017);
+
+INSERT INTO GENRES VALUES (5006, 'Drama');
+INSERT INTO GENRES VALUES (5006, 'Horror');
+INSERT INTO GENRES VALUES (5006, 'Thriller');
+
+INSERT INTO LOCATIONS VALUES (5006, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5006, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5006, 'Google Play');
+
+INSERT INTO DIRECTORS VALUES (5006, 'Andy', 'Muschietti');
+
+INSERT INTO ROLES VALUES (5006, 119, 'Bill', 'Skargard', 'Pennywise');
+INSERT INTO ROLES VALUES (5006, 120, 'Jaden', 'Lieberher', 'Bill Denbrough');
+INSERT INTO ROLES VALUES (5006, 121, 'Finn', 'Wolfhard', 'Richie Tozier');
+
+
+--REM MOVIE 8 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5007, 'Jigsaw', 'Bodies are turning up around the city, each having met a 
+				uniquely gruesome demise. As the investigation proceeds, evidence points to 
+				one suspect: John Kramer, the man known as Jigsaw, who has been dead for ten 
+				years.', 92, 'R', 34, 5.9, 2017);
+
+INSERT INTO GENRES VALUES (5007, 'Crime');
+INSERT INTO GENRES VALUES (5007, 'Horror');
+INSERT INTO GENRES VALUES (5007, 'Mystery');
+
+INSERT INTO LOCATIONS VALUES (5007, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5007, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5007, 'Google Play');
+
+INSERT INTO DIRECTORS VALUES (5007, 'Michael', 'Spierig');
+
+INSERT INTO ROLES VALUES (5007, 122, 'Matt', 'Passmore', 'Logan Nelson');
+INSERT INTO ROLES VALUES (5007, 123, 'Tobin', 'Bell', 'Jigsaw');
+INSERT INTO ROLES VALUES (5007, 124, 'Callum Keith', 'Rennie', 'Detective Halloran');
+
+
+--REM MOVIE 9 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5008, 'Pitch Perfect 3', 'Following their win at the world championship, the
+				now separated Bellas reunite for one last singing competition at an overseas 
+				USO tour, but face a group who uses both instruments and voices.', 93, 'PG-13',
+				31, 6.2, 2017);
+
+INSERT INTO GENRES VALUES (5008, 'Comedy');
+INSERT INTO GENRES VALUES (5008, 'Music');
+
+INSERT INTO LOCATIONS VALUES (5008, 'Theater');
+
+INSERT INTO DIRECTORS VALUES (5008, 'Trish', 'Sie');
+
+INSERT INTO ROLES VALUES (5008, 125, 'Anna', 'Kendrick', 'Beca');
+INSERT INTO ROLES VALUES (5008, 126, 'Rebel', 'Wilson', 'Fat Amy');
+INSERT INTO ROLES VALUES (5008, 127, 'Brittany', 'Snow', 'Chloe');
+
+--REM MOVIE 10 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5009, 'Kingsman: The Golden Circle', 'When their headquarters are destroyed and 
+				the world is held hostage, the Kingsman''s journey leads them to the discovery of 
+				an allied spy organization in the US. These two elite secret organizations must 
+				band together to defeat a common enemy.', 141, 'R', 52, 6.9, 2017);
+
+INSERT INTO GENRES VALUES (5009, 'Action');
+INSERT INTO GENRES VALUES (5009, 'Adventure');
+INSERT INTO GENRES VALUES (5009, 'Comedy');
+
+INSERT INTO LOCATIONS VALUES (5009, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5009, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5009, 'Google Play');
+
+INSERT INTO DIRECTORS VALUES (5009, 'Matthew', 'Vaughn');
+
+INSERT INTO ROLES VALUES (5009, 128, 'Taron', 'Egerton', 'Eggsy');
+INSERT INTO ROLES VALUES (5009, 129, 'Colin', 'Firth', 'Harry Hart');
+INSERT INTO ROLES VALUES (5009, 130, 'Mark', 'Strong', 'Merlin');
+
+--REM MOVIE 11 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5010, 'The Dark Knight', 'When the menace known as the Joker emerges from his mysterious
+				past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one 
+				of the greatest psychological and physical tests of his ability to fight injustice.', 152, 
+				'PG-13', 94, 9.0, 2008);
+
+INSERT INTO GENRES VALUES (5010, 'Action');
+INSERT INTO GENRES VALUES (5010, 'Crime');
+INSERT INTO GENRES VALUES (5010, 'Thriller');
+
+INSERT INTO LOCATIONS VALUES (5010, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5010, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5010, 'Google Play');
+INSERT INTO LOCATIONS VALUES (5010, 'iTunes');
+INSERT INTO LOCATIONS VALUES (5010, 'Vudu');
+
+INSERT INTO DIRECTORS VALUES (5010, 'Christopher', 'Nolan');
+
+INSERT INTO ROLES VALUES (5010, 131, 'Christian', 'Bale', 'Bruce Wayne');
+INSERT INTO ROLES VALUES (5010, 132, 'Heath', 'Ledger', 'Joker');
+INSERT INTO ROLES VALUES (5010, 133, 'Aaron', 'Eckhart', 'Harvey Dent');
+
+--REM MOVIE 12 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5011, 'Pulp Fiction', 'The lives of two mob hitmen, a boxer, a gangster''s wife, and a pair
+				of diner bandits intertwine in four tales of violence and redemption.', 154, 'R', 94, 8.9, 1994);
+
+INSERT INTO GENRES VALUES (5011, 'Crime');
+INSERT INTO GENRES VALUES (5011, 'Drama');
+
+INSERT INTO LOCATIONS VALUES (5011, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5011, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5011, 'Google Play');
+INSERT INTO LOCATIONS VALUES (5011, 'iTunes');
+INSERT INTO LOCATIONS VALUES (5011, 'Vudu');
+
+INSERT INTO DIRECTORS VALUES (5011, 'Quentin', 'Tarantino');
+
+INSERT INTO ROLES VALUES (5011, 134, 'John', 'Travolta', 'Vincent Vega');
+INSERT INTO ROLES VALUES (5011, 135, 'Uma', 'Thurman', 'Mia Wallce');
+INSERT INTO ROLES VALUES (5011, 136, 'Samuel L.', 'Jackson', 'Jules Winnfield');
+
+--REM MOVIE 13 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5012, 'Spirited Away', 'During her family''s move to the suburbs, a sullen 10-year-old
+				girl wanders into a world ruled by gods, witches, and spirits, and where humans are 
+				changed into beasts.', 125, 'PG', 97, 8.6, 2001);
+
+INSERT INTO GENRES VALUES (5012, 'Animation');
+INSERT INTO GENRES VALUES (5012, 'Adventure');
+INSERT INTO GENRES VALUES (5012, 'Family');
+
+INSERT INTO LOCATIONS VALUES (5012, 'Amazon Video');
+
+INSERT INTO DIRECTORS VALUES (5012, 'Hayao', 'Miyazaki');
+
+INSERT INTO ROLES VALUES (5012, 137, 'Daveigh', 'Chase', 'Chihiro');
+INSERT INTO ROLES VALUES (5012, 138, 'Suzanne', 'Pleshette', 'Yubaba / Zeniba');
+INSERT INTO ROLES VALUES (5012, 139, 'Miyu', 'Irino', 'Haku');
+
+--REM MOVIE 14 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5013, 'The Green Mile', 'The lives of guards on Death Row are affected by one of their
+				charges: a black man accused of child murder and rape, yet who has a mysterious gift.', 
+				189, 'R', 80, 8.5, 1999);
+
+INSERT INTO GENRES VALUES (5013, 'Crime');
+INSERT INTO GENRES VALUES (5013, 'Drama');
+INSERT INTO GENRES VALUES (5013, 'Fantasy');
+
+
+INSERT INTO LOCATIONS VALUES (5013, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5013, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5013, 'Google Play');
+INSERT INTO LOCATIONS VALUES (5013, 'iTunes');
+INSERT INTO LOCATIONS VALUES (5013, 'Vudu');
+
+INSERT INTO DIRECTORS VALUES (5013, 'Frank', 'Darabont');
+
+INSERT INTO ROLES VALUES (5013, 140, 'Tom', 'Hanks', 'Paul Edgecomb');
+INSERT INTO ROLES VALUES (5013, 141, 'Michael Clarke', 'Duncan', 'John Coffey');
+INSERT INTO ROLES VALUES (5013, 142, 'David', 'Morse', 'Brutus Howell');
+
+--REM MOVIE 15 ========================================================
+
+INSERT INTO 	MOVIES
+VALUES			(5014, 'Monsters, Inc', 'In order to power the city, monsters have to scare children so that
+				they scream. However, the children are toxic to the monsters, and after a child gets through, 
+				2 monsters realize things may not be what they think.', 92, 'G', 96, 8.1, 2001);
+
+INSERT INTO GENRES VALUES (5014, 'Animation');
+INSERT INTO GENRES VALUES (5014, 'Adventure');
+INSERT INTO GENRES VALUES (5014, 'Comedy');
+
+
+INSERT INTO LOCATIONS VALUES (5014, 'Amazon Video');
+INSERT INTO LOCATIONS VALUES (5014, 'YouTube');
+INSERT INTO LOCATIONS VALUES (5014, 'Google Play');
+INSERT INTO LOCATIONS VALUES (5014, 'iTunes');
+INSERT INTO LOCATIONS VALUES (5014, 'Vudu');
+
+INSERT INTO DIRECTORS VALUES (5014, 'Pete', 'Docter');
+
+INSERT INTO ROLES VALUES (5014, 143, 'Billy', 'Crystal', 'Mike');
+INSERT INTO ROLES VALUES (5014, 144, 'John', 'Goodman', 'Sullivan');
+INSERT INTO ROLES VALUES (5014, 145, 'Mary', 'Gibbs', 'Boo');
 
