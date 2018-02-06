b210960436a2d19f9034779f0ca546aaddda6cc0
Jasmine Dacones <jasminedacones@gmail.com>
2018-02-05 20:55:50 -0800

    Provided documentation for the ten queries

--- gmcQueries.sql
+++ gmcQueries.sql
@@ -1,94 +1,126 @@
-﻿--REM
---DONE
---SQL Query 1: Computes a join of at least three tables
---Show all comedies from 2017 that are still in theaters
+﻿--  -----------------------------------------------------------------------------------
+--  -----------------------------------------------------------------------------------
+--	PART C
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 1
+--	Purpose:	Retrieves movie titles
+--	Expected:	A table displaying the title of 2017 movies that are still in theaters
 
-SELECT title AS "2017 comedy movies still in theaters"
-FROM GENRES JOIN MOVIES ON movie_id = id JOIN LOCATIONS ON id = LOCATIONS.movie_id
-WHERE genre = 'Comedy' AND release_year = 2017 AND location = 'Theaters'
+	SELECT title AS "2017 comedy movies still in theaters"
+	FROM GENRES JOIN MOVIES ON movie_id = id JOIN LOCATIONS ON id = LOCATIONS.movie_id
+	WHERE genre = 'Comedy' AND release_year = 2017 AND location = 'Theaters'
 
---DONE
---SQL Query 2: Uses nested queries with the ANY or ALL operator and uses a GROUP BY clause
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 2
+--	Purpose:	Retrieves the release year, title, and the user ratings for movies
+--	Expected:	A table displaying movies with a user rating above an 8 (out of 10)
+--				and grouped according to the release year, the title, and the user
+--				rating the movie received
 
---SELECT M.title, release_year
---FROM MOVIES AS M
---WHERE user_rating >= ALL (SELECT user_rating FROM MOVIES WHERE release_year = 2016) AND release_year = 2016
---GROUP BY release_year;
+	SELECT release_year, title, user_rating
+	FROM MOVIES
+	WHERE user_rating > ALL
+			(SELECT user_rating FROM MOVIES WHERE user_rating < 8)
+			GROUP BY release_year, title, user_rating
 
---SELECT M.title, release_year
---FROM MOVIES AS M
---WHERE user_rating >= ALL (SELECT user_rating, release_year FROM MOVIES) AND release_year = 2016
---GROUP BY release_year;
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 3
+--	Purpose:	Retrieves all attributes from the MOVIES relation.
+--	Expected:	A table displaying movies that received a user_rating higher than the
+--				average user rating of all the movies (average: 7.713333)
 
---SELECT title
---FROM MOVIES JOIN GENRES ON id = movie_id
---WHERE GENRES.genre = 'Comedy'
+	SELECT * 
+	FROM MOVIES
+	WHERE user_rating > (SELECT AVG(user_rating) FROM MOVIES);
 
-Select release_year, title, user_rating
-From Movies
-Where user_rating > ALL
- (Select user_rating From Movies where user_rating < 8)
- Group by release_year, title, user_rating
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 4
+--	Purpose:	Retrieves the title of a movie and the director's name
+--	Expected:	A table displaying the titles of movies and the full name of directed
+--				each one
 
+	SELECT M.title AS Movies, D.fname AS "First", D.lname AS "Last"
+	FROM MOVIES AS M FULL JOIN DIRECTORS AS D ON M.id = D.movie_id;
 
---DONE
---SQL Query 3: A correlated nested query
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 5
+--	Purpose:	Retrieves the title of a movie, its user rating, and whether the rating
+--				is considered a high or low rating
+--	Expected:	A table displaying all movies and an indication that the movie is highly
+--				(ratings >= 8) or lowly rated (ratings < 8)
 
-SELECT *
-FROM MOVIES AS M1
-WHERE M1.user_rating > (SELECT AVG(user_rating) FROM MOVIES);
+	SELECT title AS "Movie", user_rating AS "User Rating", 'High' AS "High/Low Rating"
+	FROM MOVIES
+	WHERE user_rating >= 8
+	UNION
+		(SELECT title, user_rating, 'Low'
+		 FROM MOVIES
+		 WHERE user_rating < 8 )
 
---DONE
---SQL Query 4: Uses a FULL JOIN
-SELECT MOVIES.title AS Movies, DIRECTORS.fname AS "First", DIRECTORS.lname AS "Last"
-FROM MOVIES FULL JOIN DIRECTORS ON MOVIES.id = DIRECTORS.movie_id;
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 6
+--	Purpose:	Retrieves the title of a movie and its Rotten Tomatoes score.
+--	Expected:	A table displaying the top action movies (Rotten Tomatoes score >= 70%) 
+--				of 2017. Ordering set to display the highest rated movies at the top
 
---DONE
---SQL Query 5: Uses nested queries with any of the set operations UNION, EXCEPT, or INTERSECT
+	SELECT title AS 'Top Action Movies of 2017', rt_rating AS 'Rotten Tomatoes Score' 
+	FROM MOVIES JOIN GENRES ON id = movie_id
+	WHERE GENRES.genre = 'Action' AND MOVIES.rt_rating >= 70 AND MOVIES.release_year = 2017
+	ORDER BY rt_rating DESC
 
-Select title, user_rating, 'High rating'
-From Movies
-Where user_rating >= 8
-UNION
-	(Select title, user_rating, 'Low rating'
-	From Movies
-	Where user_rating < 8 )
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 7
+--	Purpose:	Retrieves the title of a movie, the locations of where the movie can be
+--				viewed, and its ratings from both users and Rotten Tomatoes
+--	Expected:	A table displaying "critically-acclaimed movies" (ratings above the
+--				average for both user ratings (7.713333) and Rotten Tomatoes ratings
+--				(75%)
 
+	SELECT M.title AS "Movie", L.location AS "Where to watch", M.user_rating 
+				   AS "User Rating", M.rt_rating AS "Rotten Tomatoes Score"
+	FROM MOVIES AS M JOIN LOCATIONS AS L ON M.id = L.movie_id
+	WHERE user_rating >= (SELECT AVG(user_rating) FROM MOVIES) 
+							AND 
+							rt_rating > = (SELECT AVG(rt_rating) FROM MOVIES);
 
---DONE
---SQL Query 6: Create your  own non-trivial SQL query
---List all action movies from 2017 with a rating of at least 70% rating from Rotten Tomatoes
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 8
+--	Purpose:	Retrieves the full details of a movie
+--	Expected:	A table displaying the highest-rated movie of 2017
 
-SELECT title AS 'Top Action Movies of 2017', rt_rating AS 'Rotten Tomatoes Score' 
-FROM MOVIES JOIN GENRES ON id = movie_id
-WHERE GENRES.genre = 'Action' AND MOVIES.rt_rating >= 70
-ORDER BY rt_rating DESC
+	SELECT *
+	FROM MOVIES
+	WHERE user_rating = (SELECT MAX(user_rating) FROM MOVIES WHERE release_year = 2017);
 
---DONE
---SQL Query 7: Create your  own non-trivial SQL query
-SELECT M.title, L.location, M.user_rating, M.rt_rating
-FROM MOVIES AS M JOIN LOCATIONS AS L ON M.id = L.movie_id
-WHERE user_rating >= (SELECT AVG(user_rating) FROM MOVIES) 
-						AND 
-						rt_rating > = (SELECT AVG(rt_rating) FROM MOVIES);
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 9
+--	Purpose:	Retrieves the full details of movies
+--	Expected:	A table displaying PG-13 movies that were released before 2017
 
-
---DONE
---SQL Query 8: Create your  own non-trivial SQL query
-SELECT *
-FROM MOVIES
-WHERE user_rating = (SELECT MAX(user_rating) FROM MOVIES WHERE release_year = 2017);
-
---DONE
---SQL Query 9: Create your  own non-trivial SQL query
-
-SELECT *
-FROM (
-	SELECT * FROM MOVIES WHERE rating = 'PG-13') as pg13Movies
-WHERE release_year < 2017
+	SELECT *
+	FROM (
+		 SELECT * FROM MOVIES WHERE rating = 'PG-13'
+		 ) as pg13movies
+	WHERE release_year < 2017
 	
---DONE
---SQL Query 10: Create your  own non-trivial SQL query
-SELECT *
-FROM MOVIES AS M
-WHERE NOT EXISTS(SELECT * FROM GENRES AS G WHERE G.movie_id = M.id AND G.genre = 'Comedy')
+--	-----------------------------------------------------------------------------------
+--	-----------------------------------------------------------------------------------
+--	QUERY 10
+--	Purpose:	Retrieves the full details of movies
+--	Expected:	A table displaying movies that aren't comedies
+
+	SELECT *
+	FROM MOVIES AS M
+	WHERE NOT EXISTS(SELECT * FROM GENRES AS G WHERE G.movie_id = M.id AND G.genre = 'Comedy')
+
+--	-----------------------------------------------------------------------------------
+--	END OF SCRIPT (February 5, 2018)
